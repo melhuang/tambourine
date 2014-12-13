57efd313b9c5dfad58f13a53b328682430af9672
Alton Zheng-Xie <altonzheng@berkeley.edu>
2014-12-13 15:45:42 -0800

    translate mml

--- util.js
+++ util.js
@@ -9,6 +9,32 @@ function determine_beats(measure) {
   }
   // return
 }
+
+function translate_mml(sounds) {
+  // Takes in an array of sounds
+  // [{notes:['A','B'], beat:4}, {notes:['A','B'], beat:4}]
+  result = ""
+  for (var sound in sounds) {
+    notes = sound['notes']
+    beat = sound['beat']
+
+    exp = ""
+    // Single note
+    if (notes.length == 1)  {
+      exp = notes[0].toLowerCase() + beat.toString();
+    } else {
+      last_note = notes.pop()
+      for (var note in notes) {
+        exp += note.toLowerCase() + "0"
+      }
+      exp += last_note + beat.toString();
+    }
+
+    result += exp + " "
+  }
+  return result
+}
+
 function chord(arr) {
   var c = "";
   for (var i = 0, i < arr.length; i++) {
