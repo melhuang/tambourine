b79d13a16baee994703d4e0e4180fbdc39121832
Melissa Huang <melissahuang10@gmail.com>
2014-12-13 15:45:23 -0800

    v1 determine beats

--- util.js
+++ util.js
@@ -3,11 +3,15 @@
 function determine_beats(measure) {
   //measure is array of sounds, get length
   //sound is object with array of notes & beats (integer)
+  var time = 4; // replace with global
   var total = 0;
   for (var sound in measure) {
     total += sound.beats;
   }
-  // return
+  for (var sound in measure) {
+    sound.beats = total / Math.pow(2,sound.beats);
+  }
+  return measure;
 }
 function chord(arr) {
   var c = "";
