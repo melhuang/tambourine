25949f25c40c0f39f01865217fcb89a9c39973ac
William Tang <williamctang93@gmail.com>
2014-12-14 02:10:39 -0800

    async calls to rparse working

--- test.js
+++ test.js
@@ -1,4 +1,7 @@
 var Q = require('./tambourine.js');
-
 // console.log(Q);
-Q.createMelody('hello');
+
+var m1 = Q.createMelody('C');
+var m2 = Q.createMelody('D');
+var m3 = Q.createMelody('B A - B-- D -- A -- | A B C D | A C D A');
+Q.play([m1,m2, m3]);
