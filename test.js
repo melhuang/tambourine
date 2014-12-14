6678269b2632404ab699250b19b0eec8b8ac38e7
Alton Zheng-Xie <altonzheng@berkeley.edu>
2014-12-14 14:06:01 -0800

    added rests

--- test.js
+++ test.js
@@ -7,6 +7,8 @@ var Q = require('./tambourine.js');
 // var m2 = Q.createMelody('D');
 
 //var m3 = Q.createMelody('B A- B-- D -- A -- | A B C D | A C D A |');
-var m1 = Q.createMelody('F# E D C# | B A B C# | A B A B A B A B | A |');
-var m2 = Q.createMelody('D C# B A  | G F# G E | C D C D C D C D | A |');
+var m1 = Q.createMelody('F# E D C# | (A, B, C) |');
+//var m2 = Q.createMelody('D C# B A  | G F# G E | C D C D C D C D | (A, B, C) |');
+var m2 = Q.createMelody('   A  --- | (A, B, C) |');
+
 Q.play([m1, m2]);
