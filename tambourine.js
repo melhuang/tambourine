36e09b2db1871f478e404819c920d355f66cb213
William Tang <williamctang93@gmail.com>
2014-12-14 02:17:13 -0800

    timbre sound generation working

--- tambourine.js
+++ tambourine.js
@@ -2,7 +2,6 @@
 var rparse = require('./rparse.js').rparse;
 
 var T = require('timbre');
-// var Join = require('join').Join;
 var forEachAsync = require('forEachAsync').forEachAsync;
 var fs = require('fs');
 
@@ -57,14 +56,26 @@ fs.readFile('grammar.grm', 'utf8', function (err, fileContent) {
 //   }
 // });
 
-/* PUBLIC METHODS */
+var env   = T("adsr", {d:3000, s:0, r:600});
+var synth = T("SynthDef", {mul:0.45, poly:8});
 
+synth.def = function(opts) {
+  var op1 = T("sin", {freq:opts.freq*6, fb:0.25, mul:0.4});
+  var op2 = T("sin", {freq:opts.freq, phase:op1, mul:opts.velocity/128});
+  return env.clone().append(op2).on("ended", opts.doneAction).bang();
+};
 
-/* FutureJS orEachAsync implementation. */
+var master = synth;
+var mod    = T("sin", {freq:2, add:3200, mul:800, kr:1});
+master = T("eq", {params:{lf:[800, 0.5, -2], mf:[6400, 0.5, 4]}}, master);
+master = T("phaser", {freq:mod, Q:2, steps:4}, master);
+master = T("delay", {time:"BPM60 L16", fb:0.65, mix:0.25}, master);
 
 var tempo = '4/4';
 var volume = 8;
 var octave = 4;
+
+/* PUBLIC METHODS */
 
 exports.setTempo = function (newTempo) {
   var patt = /^([1-9])+\/([1-9])+$/g;
@@ -131,6 +142,9 @@ exports.play = function (melodies) {
   }).then(function(){
     console.log('all requests have finished');
     console.log('mmlNotes: ' + mmlNotes);
+    T("mml", {mml:[mmlNotes]}, synth).on("ended", function() {
+      this.stop();
+    }).set({buddies:master}).start().play();
   });
 
   // for (index in melodies) {
