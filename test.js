65cbb1466289c724dca2dc51997a18632057e5ec
William Tang <williamctang93@gmail.com>
2014-12-14 02:41:59 -0800

    repeats for melodies

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 var Q = require('./tambourine.js');
 // console.log(Q);
 
-var m1 = Q.createMelody('C');
-var m2 = Q.createMelody('D');
-var m3 = Q.createMelody('B A - B-- D -- A -- | A B C D | A C D A');
-Q.play([m1,m2, m3]);
+var m1 = Q.createMelody('C D | E F G');
+var m2 = Q.createMelody('E F | G A B');
+m1.repeat(2);
+// var m2 = Q.createMelody('D');
+// var m3 = Q.createMelody('B A - B-- D -- A -- | A B C D | A C D A');
+Q.play([m1,m2]);
