dc10756fd42c29d7db7d758bad4f09f799bf4d1b
William Tang <williamctang93@gmail.com>
2014-12-14 17:27:14 -0800

    pluck working and syntax diagram

--- tambourine.js
+++ tambourine.js
@@ -32,7 +32,7 @@ exports.setEnv = function(attack, decay, sustain, release){
   env = T("adsr", {a:attack, d:decay, s:sustain, r:release});
 }
 
-module.exports.SynthType = {
+global.SynthTypes = {
   SIN: "sin",
   PLUCK: "pluck"
 };
@@ -40,9 +40,14 @@ module.exports.SynthType = {
 exports.setSynth = function (type, fb, mul) {
   synth.def = function(opts) {
     var op1 = T(type, {freq:opts.freq, fb:fb, mul:mul});
-    var op2 = T(type, {freq:opts.freq, phase:op1, mul:opts.velocity/128});
+    var op2 = T("sin", {freq:opts.freq, phase:op1, mul:opts.velocity/128});
     return env.clone().append(op2).on("ended", opts.doneAction).bang();
   }
+  master = synth;
+  var mod    = T("sin", {freq:2, add:3200, mul:800, kr:1});
+  master = T("eq", {params:{lf:[800, 0.5, -2], mf:[6400, 0.5, 4]}}, master);
+  master = T("phaser", {freq:mod, Q:2, steps:4}, master);
+  master = T("delay", {time:"BPM60 L16", fb:0.65, mix:0.25}, master);
 }
 
 /* SONG SETTINGS */
--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 var Q = require('./tambourine.js');
 // console.log(Q);
 
-// var m1 = Q.createMelody('C D | E F G');
-// var m2 = Q.createMelody('E F | G A B');
-// m1.repeat(2);
+var m1 = Q.createMelody('C D | E F G|');
+var m2 = Q.createMelody('E F | G A B|');
+m1.repeat(2);
+Q.setSynth(SynthTypes.PLUCK, .25, .4);
 // var m2 = Q.createMelody('D');
 
+console.log(SynthTypes.PLUCK);
 //var m3 = Q.createMelody('B A- B-- D -- A -- | A B C D | A C D A |');
-var m1 = Q.createMelody('F# E / / / / D C# | B A B C# | A B A B A B A B | A |');
-var m2 = Q.createMelody('D C# B A  | G F# G E | C D C D C D C D | A |');
 Q.play([m1, m2]);
